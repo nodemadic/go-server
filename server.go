5577bfbcdf684245f4f82c323240cd0699de9a57
nodemadic <nodemadic@gmail.com>
2022-12-13 22:06:10 +0000

    Adds a handler function with error catching

--- server.go
+++ server.go
@@ -8,12 +8,26 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello!")
-	})
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func helloHandler (w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!")
 }
