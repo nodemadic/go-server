a7920215590f90c7900710a39feb1485cbe2930a
nodemadic <nodemadic@gmail.com>
2022-12-14 19:58:40 +0000

    Creates a form handler function

--- server.go
+++ server.go
@@ -8,6 +8,9 @@ import (
 )
 
 func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
@@ -30,4 +33,17 @@ func helloHandler (w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Hello!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
